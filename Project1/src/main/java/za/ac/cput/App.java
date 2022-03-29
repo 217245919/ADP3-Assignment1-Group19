c8f847b943d545c3c6c1b3bef3ad58a1bf14dd71
217245919 <lylehaines.lh.lh@gmail.com>
2022-03-29 17:00:15 +0200

    Maven project created
    Pom file updated

---
+++ Project1/src/main/java/za/ac/cput/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
