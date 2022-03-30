4fe56bc26c87f6f5bae38be9d468116b4b86e743
potatogallery <khazsaunders@hotmail.com>
2022-03-30 10:51:03 +0200

    TDD small Program
    Section A Small Program

---
+++ Project1/src/main/java/za/ac/cput/CarIg.java
@@ -0,0 +1,20 @@
+/*
+* Keallan Saunders 219169357
+* SECTION A :::::: SMALL TDD Program
+* 30 March 2022
+* CarIg.java   +   CarIgTest
+* ADP3 ASSIGNMENT 1
+* */
+
+
+package za.ac.cput;
+
+public class CarIg {
+    public double getIgon() {
+        return 0;
+    }
+
+    public double getIgOff() {
+        return 5;
+    }
+}
---
+++ Project1/src/test/java/za/ac/cput/CarIgTest.java
@@ -0,0 +1,29 @@
+/*
+ * Keallan Saunders 219169357
+ * SECTION A :::::: SMALL TDD Program
+ * 30 March 2022
+ * CarIgTest.java      +     CarIg.java
+ * ADP3 ASSIGNMENT 1
+ * */
+
+package za.ac.cput;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarIgTest {
+    @Test
+    public void ignitionOn(){
+        CarIg cg = new CarIg();
+        assertEquals(0.0, cg.getIgon(), 0.0);
+        System.out.println("VW Ignition is Now Turned On");
+
+    }
+    @Test
+    public void ignitionOff(){
+        CarIg bb = new CarIg();
+        assertEquals(5.0, bb.getIgOff(), 0.0);
+        System.out.println("VW Igntion is now Turned Off");
+    }
+}
