93b5155d0161fc65f716756500d961e62f4be331
potatogallery <khazsaunders@hotmail.com>
2022-04-06 17:42:16 +0200

    DomainClassessUpdate
    Bookings+Location Classes Added

---
+++ Project1/src/main/java/za/ac/cput/domain/Location.java
@@ -0,0 +1,10 @@
+/*
+Keallan Saunders 219169357
+ADP3 ASSIGNMENT1
+DD: 10 APRIL 2022
+Location.java
+**************** DO NOT TOUCH ********************
+ */package za.ac.cput.domain;
+
+public class Location {
+}
