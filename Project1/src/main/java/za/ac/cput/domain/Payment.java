c81ca32be82abbdb2c3dea2de9db37038f318325
217245919 <lylehaines.lh.lh@gmail.com>
2022-04-07 12:13:22 +0200

    Added code to my entities
    Coded customer and payment entities

--- Project1/src/main/java/za/ac/cput/domain/Payment.java
+++ Project1/src/main/java/za/ac/cput/domain/Payment.java
@@ -4,8 +4,87 @@ package za.ac.cput.domain;
   Payment.java
   Entity for the Payment
   Lyle Haines (217245919)
-  06 April 2022
+  10 April 2022
  */
 
+import java.util.Date;
+
 public class Payment {
+
+    private int paymentId;
+    private int totalAmount;
+    private Date date;
+
+    private Payment(Builder builder) {
+        this.paymentId = builder.paymentId;
+        this.totalAmount = builder.totalAmount;
+        this.date = builder.date;
+
+    }
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "paymentId=" + paymentId +
+                ", totalAmount=" + totalAmount +
+                ", date=" + date +
+                '}';
+    }
+
+    public static class Builder {
+
+        private int paymentId;
+        private int totalAmount;
+        private Date date;
+
+        public Builder setPaymentId(int paymentId) {
+            this.paymentId = paymentId;
+            return this;
+        }
+
+        public Builder setTotalAmount(int totalAmount) {
+            this.totalAmount = totalAmount;
+            return this;
+        }
+
+        public Builder setDate(Date date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder copy(Payment payment) {
+            this.paymentId = payment.paymentId;
+            this.totalAmount = payment.totalAmount;
+            this.date = payment.date;
+            return this;
+        }
+
+        public Payment build() {
+            return new Payment(this);
+        }
+    }
 }
--- Project1/src/main/java/za/ac/cput/factory/CustomerFactory.java
+++ Project1/src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -4,7 +4,7 @@ package za.ac.cput.factory;
   CustomerFactory.java
   Factory for the Customer
   Lyle Haines (217245919)
-  06 April 2022
+  10 April 2022
  */
 
 public class CustomerFactory {
