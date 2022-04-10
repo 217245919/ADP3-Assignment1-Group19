7436be40ba022b667a4b07d58e58d925ff04af16
RedarBeh <80404526+RedarBeh@users.noreply.github.com>
2022-04-10 20:30:45 +0200

    Factory classes done

---
+++ Project1/src/main/java/za/ac/cput/factory/DeliveryBoyFactory.java
@@ -0,0 +1,23 @@
+/*
+Mogammad-Redar Behardien 216234107
+ADP3 ASSIGNMENT1
+DD: 10 APRIL 2022
+DeliveryBoyFactory.java
+**************** DO NOT TOUCH ********************
+ */
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.DeliveryBoy;
+
+
+public class DeliveryBoyFactory {
+    public static DeliveryBoy createDeliveryBoy(String delivBId, String delivBFname, String delivBSname, String delivBCellNr){
+        DeliveryBoy deliveryBoy= new DeliveryBoy.Builder()
+                .setDelivBId(delivBId)
+		.setDelivBFname(delivBFname)
+                .setDelivBSname(delivBSname)
+                .setDelivBCellNr(delivBCellNr)
+                .build();
+        return deliveryBoy;
+    }
+}
